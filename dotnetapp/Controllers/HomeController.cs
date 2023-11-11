b3877b9aad317e3c02c8dd8c011015d7ba79face
Iamneo <techadmin@iamneo.ai>
2023-11-11 04:46:47 +0000

    Backup Commit

---
+++ dotnetapp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using dotnetapp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using dotnetapp.Models;
+
+public class HomeController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public HomeController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var featuredBooks = _context.Books.ToList();
+        return View(featuredBooks);
+    }
+}
